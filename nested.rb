4418749902fa2ac07845620f73d369da0dfddf5b
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-23 21:11:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -101,6 +101,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
     programmer_hash[:alan_kay][:known_for][0] = "GUI"
+    programmer_hash
      
 end
 
@@ -121,4 +122,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash
 end
